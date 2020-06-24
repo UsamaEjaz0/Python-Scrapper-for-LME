1a7a0d517d7423fe78c0c86061b509778648c35a
UsamaEjaz0 <uejaz0@gmail.com>
2020-06-24 21:11:44 +0500

    currency convert done

--- Scrapper.py
+++ Scrapper.py
@@ -1,8 +1,27 @@
 import csv
 import requests
 from bs4 import BeautifulSoup as bs
+
 from pandas.io.html import read_html
 import lxml
+
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://www.x-rates.com/calculator/?from=USD&to=PKR&amount=1')
+soup = BeautifulSoup(page.text, 'html.parser')
+
+part1 = soup.find(class_="ccOutputTrail").previous_sibling
+part2 = soup.find(class_="ccOutputTrail").get_text(strip=True)
+rate = "{}{}".format(part1,part2)
+
+def usd_to_pkr(rate,gbp):
+    dollars=gbp*rate
+    return dollars
+
+gbp = input("Enter USD amount: ")
+finalamt = usd_to_pkr(float(rate),float(gbp))
+print(str(gbp)+" USD is equvalent to "+str(finalamt)+" PKR")
 # url = "https://www.lme.com/"
 #
 #
--- currency rates.py
+++ currency rates.py
@@ -1,17 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get('https://www.x-rates.com/calculator/?from=GBP&to=USD&amount=1')
+page = requests.get('https://www.x-rates.com/calculator/?from=USD&to=PKR&amount=1')
 soup = BeautifulSoup(page.text, 'html.parser')
 
 part1 = soup.find(class_="ccOutputTrail").previous_sibling
 part2 = soup.find(class_="ccOutputTrail").get_text(strip=True)
 rate = "{}{}".format(part1,part2)
 
-def gbp_to_usd(rate,gbp):
+def usd_to_pkr(rate,gbp):
     dollars=gbp*rate
     return dollars
 
-gbp = input("Enter GBP amount: ")
-finalamt = gbp_to_usd(float(rate),float(gbp))
-print(str(gbp)+" GBP is equvalent to "+"$"+str(finalamt)+" USD")
+gbp = input("Enter USD amount: ")
+finalamt = usd_to_pkr(float(rate),float(gbp))
+print(str(gbp)+" USD is equvalent to "+str(finalamt)+" PKR")
